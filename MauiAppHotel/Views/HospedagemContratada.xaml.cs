ab7fb1f08b9c394e012dcea2c6583a42cdff212a
Usuário <Usurio@USER>
2024-11-16 21:54:57 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiAppHotel.Views
+{
+    public partial class HospedagemContratada : ContentPage
+    {
+        public HospedagemContratada()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
